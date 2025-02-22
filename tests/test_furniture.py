1e3ecb6e29d0ad8e0a0c3fcbb4b93be69741d64b
Your Name <hadasic123@gmail.com>
2025-02-22 12:28:04 +0200

    fixed typo second time

--- tests/test_furniture.py
+++ tests/test_furniture.py
@@ -7,6 +7,7 @@ from furniture import Sofa
 from furniture import Wardrobe
 from furniture import Bed
 
+
 # @pytest.mark.parametrize("price", [2,-5,444])
 # def test_no_discount_calculate_discount(price):
 #     f = NoDiscount()
@@ -16,46 +17,59 @@ from furniture import Bed
 def test_apply_discount():
     pass
 
+
 def test_set_discount_strategy():
     pass
+
 
 def test_apply_tax():
     pass
 
+
 def test_is_available():
     pass
+
 
 ## Chair
 def test_chair_calculate_discount():
     pass
 
+
 def test_chair_info():
     pass
+
 
 ## Table
 def test_table_calculate_discount():
     pass
 
+
 def test_table_info():
     pass
+
 
 ## Sofa
 def test_sofa_calculate_discount():
     pass
 
+
 def test_sofa_info():
     pass
+
 
 ## Bed
 def test_bed_calculate_discount():
     pass
 
+
 def test_bed_info():
     pass
+
 
 ## Wardrobe
 def test_wardrobe_calculate_discount():
     pass
+
 
 def test_wardrobe_info():
     pass
--- tests/test_furniture_discount.py
+++ tests/test_furniture_discount.py
@@ -15,21 +15,21 @@ def test_no_discount_calculate_discount(price):
     assert result == 0
 
 
-@pytest.mark.parametrize("price, expected", [(2, 0.3),(-5, -0.75),(445, 66.75)])
+@pytest.mark.parametrize("price, expected", [(2, 0.3), (-5, -0.75), (445, 66.75)])
 def test_holiday_discount_calculate_discount(price, expected):
     f = HolidayDiscount()
     assert expected == f.calculate_discount(price)
 
 
-@pytest.mark.parametrize("price, expected", [(2, 0.4),(-5, -1),(445, 89)])
+@pytest.mark.parametrize("price, expected", [(2, 0.4), (-5, -1), (445, 89)])
 def test_vip_discount_calculate_discount(price, expected):
-    f = VIPDiscount ()
+    f = VIPDiscount()
     assert expected == f.calculate_discount(price)
 
 
-@pytest.mark.parametrize("price, expected", [(2, 0.6),(-5, -1.5),(445, 133.5)])
+@pytest.mark.parametrize("price, expected", [(2, 0.6), (-5, -1.5), (445, 133.5)])
 def test_clearance_discount_calculate_discount(price, expected):
-    f = ClearanceDiscount ()
+    f = ClearanceDiscount()
     assert expected == f.calculate_discount(price)
 
 
